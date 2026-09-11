3175c8a20789c49627a80a31886734d5d540c790
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-11 20:29:56 -0300

    feat: adiciona exercicio Processamento de pedidos

---
+++ javascript/modulo-01-loops/exercicios-for/ex2.js
@@ -0,0 +1,6 @@
+//Processamento de pedidos
+for (let pedido = 1; pedido <= 10; pedido++) {
+    console.log(`Processando pedido ${pedido}...`);
+}
+
+console.log("Todos os pedidos foram processados.");
