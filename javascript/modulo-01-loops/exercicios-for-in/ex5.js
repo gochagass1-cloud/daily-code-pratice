e4e2ec0629bad5dc2ab0c2ef65284ece3cd76d81
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-11 20:50:12 -0300

    feat: adiciona exercicio Inventario

---
+++ javascript/modulo-01-loops/exercicios-for-in/ex5.js
@@ -0,0 +1,12 @@
+//Inventario
+let equipamentos = [
+    "Computador",
+    "Impressora",
+    "Scanner",
+    "Projetor",
+    "Roteador"
+];
+
+for (let indice in equipamentos) {
+    console.log(`Posição ${indice} → ${equipamentos[indice]}`);
+}
