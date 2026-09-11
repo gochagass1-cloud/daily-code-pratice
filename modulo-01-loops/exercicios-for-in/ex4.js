e3001fd3bfab7cb36d9851a85c0b19ad1d113bc7
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-11 19:07:55 -0300

    feat: adiciona exercicio Indices de uma lista

---
+++ modulo-01-loops/exercicios-for-in/ex4.js
@@ -0,0 +1,7 @@
+//Indices de uma lista
+let produtos = ['Notebook', 'Mouse', 'Teclado', 'Monitor']
+
+for (const indice in produtos) {
+    console.log(indice);
+    console.log(produtos[indice]);
+}
