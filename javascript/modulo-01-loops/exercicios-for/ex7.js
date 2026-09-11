6bfcfd218f5ba893173155436b966746aed34d0f
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-11 20:33:37 -0300

    feat: adiciona exercicio Identificação de produtos

---
+++ javascript/modulo-01-loops/exercicios-for/ex7.js
@@ -0,0 +1,6 @@
+//Identificação de produto
+let produtos = ["Notebook", "Mouse", "Teclado", "Monitor", "Headset"];
+
+for (let i = 0; i < produtos.length; i++) {
+    console.log(`Posição ${i}: ${produtos[i]}`);
+}
