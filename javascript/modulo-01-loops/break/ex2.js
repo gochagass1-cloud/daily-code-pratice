9fb3d19a584b8e955bc381637f889482458fcfc5
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-15 18:22:00 -0300

    feat: adiciona exercicio Encontrar funcionario

---
+++ javascript/modulo-01-loops/break/ex2.js
@@ -0,0 +1,9 @@
+//Encontrar funcionario
+let funcionarios = ['Ana', 'Carlos', 'Mariana', 'Lucas', 'Pedro']
+
+for (const funcionario of funcionarios) {
+    if (funcionario === ('Mariana')) {
+        console.log(`Funcionaria encontrada: ${funcionario}`);
+        break
+    }
+}
