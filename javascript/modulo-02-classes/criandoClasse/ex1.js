b6f599039046ac44c665cf064d8f78ae5cef92de
Gabriel Oliveira Chagas <gochagass1@gmail.com>
2026-09-16 20:10:21 -0300

    feat: adiciona exercicio Cadastro de produto com Classe

---
+++ javascript/modulo-02-classes/criandoClasse/ex1.js
@@ -0,0 +1,14 @@
+//Cadastro de produto
+class Funcionario {
+    constructor(nome, cargo, setor) {
+        this.nome = nome
+        this.cargo = cargo
+        this.setor = setor
+    }
+}
+
+const funcionario1 = new Funcionario('Gabriel', 'Analista', 'TI')
+const funcionario2 = new Funcionario('Miguel', 'Desenvolvedor J', 'TI')
+
+console.log(funcionario1);
+console.log(funcionario2);
